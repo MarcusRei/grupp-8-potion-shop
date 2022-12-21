12d3ef978343de0fc69cf8573fc76cef002af6c5
jennifer-mcallister <jennifer_mca@hotmail.com>
2022-12-21 23:10:54 +0100

    added function to calculate total price

---
+++ src/ts/totalAmount.ts
@@ -0,0 +1,26 @@
+
+import { userCart } from "./checkout_page";
+
+// summerar priset på varor i varukorgen
+function sumTotalPrice () {
+    let sum :number = 0;
+    let pTag = document.getElementById("sumOfShoppingCart") as HTMLParagraphElement;
+
+    for(let i = 0; i < userCart.length; i++) {
+
+        let amount :number= 1;
+        if (amount > 1) {
+            sum = sum + (userCart[i].price * userCart[i].quantity);
+        }
+        else {
+            sum = sum + userCart[i].price;
+        }
+    }
+
+    // skriver ut total priset i DOM
+    let totalPrice :string = sum.toString(); 
+    pTag.innerText = totalPrice + " :-";
+
+}
+
+sumTotalPrice();
