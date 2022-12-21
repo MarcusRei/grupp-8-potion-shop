0dc21896ccb6f7035a1ed879645735855a5f5c7c
Carolina Warntorp <carolina.warntorp@outlook.com>
2022-12-21 09:26:59 +0100

    add shoppingCart to html functionality

--- src/ts/checkout_page.ts
+++ src/ts/checkout_page.ts
@@ -1,3 +1,5 @@
+import { ProductTemplate } from "./models/ProductTemplate";
+
 //modal function
 export function startModalFunctionality() {
   var checkoutBtn = document.getElementById("checkoutBtn") as HTMLButtonElement;
@@ -20,4 +22,53 @@ function changeModalVisability() {
 
   modal.classList.toggle("modalVisible");
   modal.classList.toggle("modalInvisible");
+}
+
+//skapa html för varukorg
+function showShoppingCart() {
+  let shoppingCartList: ProductTemplate[] = [];
+
+  let shoppingCart = document.getElementById("shoppingCart") as HTMLElement;
+  shoppingCart.innerHTML = "";
+
+  for (let i = 0; i < shoppingCartList.length; i++) {
+    let itemRow = document.createElement("article");
+    itemRow.classList.add("itemRow");
+    let productImg = document.createElement("img");
+    productImg.classList.add("ItemRow__Img");
+    //productImg.alt = sh[i].name;
+    //productImg.src = sh[i].img;
+    let productName = document.createElement("p");
+    productName.classList.add("ItemRow__productName");
+    //productName.innerText = sh[i].name;
+    let productPrice = document.createElement("p");
+    productPrice.classList.add("ItemRow__productPrice");
+    //productPrice.innerText = sh[i].price.toString();
+    let productLabel = document.createElement("label");
+    productLabel.htmlFor = "productQuantity"; //vet inte om denna blir rätt
+    productLabel.id = "quantityContainer";
+    let productQuantityInput = document.createElement("input");
+    productQuantityInput.type = "number";
+    productQuantityInput.id = "productQuantity";
+    let btnContainer = document.createElement("div");
+    btnContainer.classList.add("ItemRow__btnContainer");
+    let btnAddQuantity = document.createElement("button");
+    btnAddQuantity.classList.add("ItemRow__changeQuantityBtn addQuantity"),
+      (btnAddQuantity.innerText = "+");
+    let btnReduceQuantity = document.createElement("button");
+    btnReduceQuantity.classList.add(
+      "ItemRow__changeQuantityBtn reduceQuantity"
+    ),
+      (btnReduceQuantity.innerText = "-");
+
+    shoppingCart.appendChild(itemRow);
+    itemRow.appendChild(productImg);
+    itemRow.appendChild(productName);
+    itemRow.appendChild(productPrice);
+    itemRow.appendChild(productLabel);
+    productLabel.appendChild(productQuantityInput);
+    productLabel.appendChild(btnContainer);
+    btnContainer.appendChild(btnAddQuantity);
+    btnContainer.appendChild(btnReduceQuantity);
+  }
 }
