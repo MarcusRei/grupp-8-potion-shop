b2e413f30c907d3780bf9e199a6114adcba1a32b
Carolina Warntorp <carolina.warntorp@outlook.com>
2022-12-21 13:27:38 +0100

    start emptyShoppingCart function

--- src/ts/checkout_page.ts
+++ src/ts/checkout_page.ts
@@ -1,5 +1,6 @@
 import { ProductTemplate } from "./models/ProductTemplate";
 // import { userCart } from "./products";
+import placeholder_square from "../assets/images/placeholder_square.jpg";
 
 //modal function
 export function startModalFunctionality() {
@@ -12,7 +13,7 @@ export function startModalFunctionality() {
     changeModalVisability();
     console.log("I got clicked");
     //lägg till function för att tömma varukorg
-    //lägg till funktion för att skapa om html
+    showShoppingCart();
   });
   closeCross.addEventListener("click", () => {
     changeModalVisability();
@@ -79,23 +80,25 @@ function showShoppingCart() {
     productLabel.appendChild(productQuantityInput);
 
     //create btn container for btns
-    let btnContainer = document.createElement("div");
-    btnContainer.classList.add("itemRow__btnContainer");
-    productLabel.appendChild(btnContainer);
+    // let btnContainer = document.createElement("div");
+    // btnContainer.classList.add("itemRow__btnContainer");
+    // productLabel.appendChild(btnContainer);
 
-    //create btn for adding quantity
-    let btnAddQuantity = document.createElement("button");
-    btnAddQuantity.classList.add("ItemRow__changeQuantityBtn"),
-      btnAddQuantity.classList.add("addQuantity"),
-      (btnAddQuantity.innerText = "+");
-    btnContainer.appendChild(btnAddQuantity);
+    // //create btn for adding quantity
+    // let btnAddQuantity = document.createElement("button");
+    // btnAddQuantity.classList.add("ItemRow__changeQuantityBtn"),
+    //   btnAddQuantity.classList.add("addQuantity"),
+    //   (btnAddQuantity.innerText = "+");
+    // btnContainer.appendChild(btnAddQuantity);
 
-    //create btn for reducing quantity
-    let btnReduceQuantity = document.createElement("button");
-    btnReduceQuantity.classList.add("itemRow__changeQuantityBtn"),
-      btnReduceQuantity.classList.add("reduceQuantity"),
-      (btnReduceQuantity.innerText = "-");
-    btnContainer.appendChild(btnReduceQuantity);
+    // //create btn for reducing quantity
+    // let btnReduceQuantity = document.createElement("button");
+    // btnReduceQuantity.classList.add("itemRow__changeQuantityBtn"),
+    //   btnReduceQuantity.classList.add("reduceQuantity"),
+    //   (btnReduceQuantity.innerText = "-");
+    // btnContainer.appendChild(btnReduceQuantity);
   }
 }
 showShoppingCart();
+
+function emptyShoppingCart() {}
