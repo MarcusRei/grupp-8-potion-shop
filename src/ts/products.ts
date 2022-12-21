4885cf941a0bf705aad6bbda442da98783eff87b
MarcusRei <marcusreineck@gmail.com>
2022-12-21 09:14:39 +0100

    image src working correctly & productlist created

--- src/ts/products.ts
+++ src/ts/products.ts
@@ -1,3 +1,4 @@
+import { products } from "./models/ProductList";
 import { ProductTemplate } from "./models/ProductTemplate";
 
 const testProducts: ProductTemplate[] = [
@@ -35,8 +36,8 @@ function addProductToCart(product: ProductTemplate) {
 }
 
 function renderProductlist() {
-  for (let i = 0; i < testProducts.length; i++) {
-    console.log(testProducts);
+  for (let i = 0; i < products.length; i++) {
+    console.log(products);
 
     //Creates container for a product
     let productContainer = document.createElement("div");
@@ -46,7 +47,7 @@ function renderProductlist() {
     //Creates image element for product
     let productImg = document.createElement("img");
     productImg.classList.add("product__container-image");
-    productImg.src = testProducts[i].image;
+    productImg.src = products[i].image;
     productImg.alt = "Picture of product";
     productContainer.appendChild(productImg);
 
@@ -63,13 +64,13 @@ function renderProductlist() {
     //Creates element for product name
     let productName = document.createElement("p");
     productName.classList.add("product__info-name");
-    productName.innerHTML = testProducts[i].name;
+    productName.innerHTML = products[i].name;
     productinfoTextContainer.appendChild(productName);
 
     //Creates element for product price
     let productPrice = document.createElement("p");
     productName.classList.add("product__info-price");
-    productPrice.innerHTML = testProducts[i].price + ":-";
+    productPrice.innerHTML = products[i].price + ":-";
     productinfoTextContainer.appendChild(productPrice);
 
     //Creates inputs container
@@ -86,21 +87,18 @@ function renderProductlist() {
 
     //Creates  "add to cart" button
     let productAddToCartBtn = document.createElement("button");
-    productAddToCartBtn.setAttribute(
-      "id",
-      `add-${testProducts[i].name}-button`
-    );
+    productAddToCartBtn.setAttribute("id", `add-${products[i].name}-button`);
     productAddToCartBtn.classList.add("product__info-buy-btn");
     productAddToCartBtn.innerText = "Add to cart";
     productInputs.appendChild(productAddToCartBtn);
 
     let productButton: HTMLButtonElement = document.getElementById(
-      `add-${testProducts[i].name}-button`
+      `add-${products[i].name}-button`
     ) as HTMLButtonElement;
 
     //Adds eventlistener
     productButton.addEventListener("click", () => {
-      addProductToCart(testProducts[i]);
+      addProductToCart(products[i]);
     });
   }
 }
