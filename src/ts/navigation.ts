24e3e569d282fcb95f2c01b223dfd06d6b712a7f
jennifer-mcallister <jennifer_mca@hotmail.com>
2022-12-21 12:05:24 +0100

    added layout for hamburgermenu

--- src/ts/navigation.ts
+++ src/ts/navigation.ts
@@ -7,16 +7,16 @@ document.getElementById("hamburgerButton")?.addEventListener("click", ()=> {
 
 function toggleHamburgerMenu () {
 
-    const menu = document.querySelector(".menu") as HTMLUListElement;
+    const mobileMenu = document.getElementById("mobilenav") as HTMLUListElement;
     const closeIcon = document.getElementById("closeIcon") as HTMLSpanElement;
     const menuIcon = document.getElementById("menuIcon") as HTMLSpanElement;
 
-    if(menu.classList.contains("showMenu")) {
-        menu.classList.remove("showMenu");
+    if(mobileMenu.classList.contains("showMenu")) {
+        mobileMenu.classList.remove("showMenu");
         closeIcon.style.display = "none";
         menuIcon.style.display = "flex";
       } else {
-        menu.classList.add("showMenu");
+        mobileMenu.classList.add("showMenu");
         closeIcon.style.display = "flex";
         menuIcon.style.display = "none";
       }
